3f7044ab9576f195a1c4708bbd9edc7d5939e786
Nick <nick@codevader.com>
2017-01-03 08:24:06 +1300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Slippers.Mixfile do
     [
       app: :slippers,
       version: "0.1.0",
-      elixir: "~> 1.1",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
